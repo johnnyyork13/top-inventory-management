075e6ba987c638b817b4a57a2ad7d9e2275935bb
johnnyyork13 <johnnyyork13@hotmail.com>
2023-10-28 20:01:23 -0400

    add item pages and routes/controllers

--- controllers/itemController.js
+++ controllers/itemController.js
@@ -1,7 +1,11 @@
 const asyncHandler= require('express-async-handler');
 const ItemSchema = require('../models/item');
 
-exports.create_item = asyncHandler((req, res, next) => {
+exports.get_add_item = asyncHandler((req, res, next) => {
+    res.render('add_item')
+})
+
+exports.post_add_item = asyncHandler(async(req, res, next) => {
     const item = new ItemSchema({
         name: req.body.name,
         description: req.body.description,
@@ -9,9 +13,18 @@ exports.create_item = asyncHandler((req, res, next) => {
         price: req.body.price,
         numItems: req.body.numItems
     })
-    res.render('all_items', {item: item})
+    await item.save();
+    const allItems = await ItemSchema.find().exec();
+    res.render('all_items', {item_list: allItems})
 })
 
-exports.get_all_items = asyncHandler((req, res, next) => {
-    res.render('all_items')
+exports.get_all_items = asyncHandler(async (req, res, next) => {
+    const allItems = await ItemSchema.find().exec();
+    res.render('all_items', {item_list : allItems});
+})
+
+exports.view_item = asyncHandler(async (req, res, next) => {
+    const item = await ItemSchema.findOne({_id: req.params.id}).exec();
+    console.log("THIS IS THE ITEM", item);
+    res.render('item_page', {item: item})
 })
--- routes/index.js
+++ routes/index.js
@@ -9,4 +9,12 @@ router.get('/', function(req, res, next) {
 
 router.get('/inventory/all', itemController.get_all_items);
 
+router.get('/inventory/add', itemController.get_add_item);
+
+router.post('/inventory/add', itemController.post_add_item);
+
+router.post('/inventory/all', itemController.post_add_item); 
+
+router.get('/inventory/:id', itemController.view_item);
+
 module.exports = router;
